dcc60e7a98a3828772b444edd7e3822152d493bb
Sumit <sumit.thecoder@gmail.com>
2020-04-25 19:53:12 +0530

    changes

---
+++ precache-manifest.3bb24fbcf498d3c419ee975dba9e399b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef078694fbc05abcf25d7a6f2c82972",
+    "url": "/covid19cg/index.html"
+  },
+  {
+    "revision": "f2c9d2d4104ac76cb58f",
+    "url": "/covid19cg/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "b155a9804f3fe40c1f6c",
+    "url": "/covid19cg/static/css/main.824813a0.chunk.css"
+  },
+  {
+    "revision": "f2c9d2d4104ac76cb58f",
+    "url": "/covid19cg/static/js/2.98183764.chunk.js"
+  },
+  {
+    "revision": "8138f1be48344c3b1007c70e326bbd31",
+    "url": "/covid19cg/static/js/2.98183764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b155a9804f3fe40c1f6c",
+    "url": "/covid19cg/static/js/main.82442a0c.chunk.js"
+  },
+  {
+    "revision": "ffaec71d7bd2dfe61062",
+    "url": "/covid19cg/static/js/runtime-main.f4fed122.js"
+  }
+]);

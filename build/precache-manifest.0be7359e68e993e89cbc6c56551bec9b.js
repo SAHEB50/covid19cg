081f348f0ec283ae2317ba8b76dd0c7cc4d68699
Sumit <sumit.thecoder@gmail.com>
2020-04-26 12:14:05 +0530

    latest build for production

---
+++ build/precache-manifest.0be7359e68e993e89cbc6c56551bec9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2af1e5bef7f91457c8644d1029a165c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4174b921b9276c3dddf7",
+    "url": "/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "5fef916abcd22c2f206b",
+    "url": "/static/css/main.824813a0.chunk.css"
+  },
+  {
+    "revision": "4174b921b9276c3dddf7",
+    "url": "/static/js/2.408db8d4.chunk.js"
+  },
+  {
+    "revision": "8138f1be48344c3b1007c70e326bbd31",
+    "url": "/static/js/2.408db8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fef916abcd22c2f206b",
+    "url": "/static/js/main.c3dfebb5.chunk.js"
+  },
+  {
+    "revision": "a97c4d12456b25e564a0",
+    "url": "/static/js/runtime-main.f7e2dbca.js"
+  }
+]);
